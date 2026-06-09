553c3d2c4afb31e24270ee13455e880e6503a27e
josiascandi445-lgtm <josiascandi445@gmail.com>
2026-06-09 20:26:32 +0100

    Create hello.js

---
+++ commands/hello.js
@@ -0,0 +1,22 @@
+export default {
+  name: "hello",
+  aliases: ["ola", "hi", "salve"],
+  description: "Mensagem de saudação do bot",
+
+  async execute({ sock, msg, jid, sender, botName }) {
+    const user = sender.split("@")[0];
+
+    const text =
+`👋 Olá!
+
+Eu sou o *${botName}*.
+Estou online e a funcionar insolente.
+Qualquer dúvida pergunta para min ou para o Bug.
+
+🤖 Bem-vindo, ${user}.
+Não teste a paciência de um zenin
+Se estiveres a testar-me, parabéns… funciono (às vezes).`;
+
+    await sock.sendMessage(jid, { text }, { quoted: msg });
+  },
+};
